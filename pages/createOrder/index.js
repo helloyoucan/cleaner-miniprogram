bd10d29a9a26cc22f8ba534d11575d271a6ea62d
wucanlong <wucanlong@bell.ai>
2021-03-29 18:29:54 +0800

    update

--- pages/createOrder/index.js
+++ pages/createOrder/index.js
@@ -5,9 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    showTopTips: false,
+    isAgree: false,
+    timeIndex:0,
+    timeList:[
+      { time:'2021/03/01 21:00-22:00'},
+      { time:'2021/03/01 22:00-23:00'},
+      { time:'2021/03/02 21:00-22:00'},
+      { time:'2021/03/02 22:00-23:00'},
+      { time:'2021/03/03 21:00-22:00'},
+      { time:'2021/03/03 22:00-23:00'},
+    ],
+    formData: {
+      region: ['广东省', '广州市', '海珠区'],
+      date: '2016-09-01',
+    }
   },
-
+  bindAgreeChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+    this.setData({
+      isAgree: e.detail.value.length > 0
+    })
+  },
+  formInputChange(e) {
+    const { field } = e.currentTarget.dataset
+    console.log(field, e.detail.value)
+    this.setData({
+      [`formData.${field}`]: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
